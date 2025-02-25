6ee4afe899c9e38d52760e426436da897daf57b8
J-Moren <kalev.1503@gmail.com>
2025-02-25 00:33:41 -0500

    Se crea Método Solicitar Dimensión

--- MatricesP7.java
+++ MatricesP7.java
@@ -1,3 +1,5 @@
+// Realice un algoritmo que llene una matriz de 5 * 6 y que imprima cuantos de los números almacenados son ceros, cuántos son positivos y cuantos son negativos
+
 public class MatricesP7 {
 
     public static void main(String[] args) {
--- Metodos.java
+++ Metodos.java
@@ -1,5 +1,21 @@
-
+import java.util.Scanner;
 
 public class Metodos {
-    
+
+    public int[][] solicitarDimension() {
+        
+        Scanner sc = new Scanner (System.in);// Crear un objeto Scanner para leer la entrada
+
+        System.out.println("Ingrese el Número de Filas de la Matriz: ");// Solicitar el Número de Filas
+        int i = sc.nextInt(); // Lee el Número de las filas
+
+        System.out.println("Ingrese el Número de las Solumnas de la Matriz: ");// Solicitar el Número de las Columnas 
+        int j = sc.nextInt(); // Lee el Número de las Columnas
+
+        int [][] matriz = new int [i][j];
+
+        sc.close();
+
+        return matriz;
+    }
 }
