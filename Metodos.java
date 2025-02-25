5b175ee35bcac72405309984f27b2d0ef6b17773
J-Moren <kalev.1503@gmail.com>
2025-02-25 01:14:07 -0500

    Se crea Método Mostrar Matriz

--- Metodos.java
+++ Metodos.java
@@ -13,19 +13,27 @@ public class Metodos {
         System.out.println("Ingrese el Número de las Solumnas de la Matriz: ");// Solicitar el Número de las Columnas 
         int j = sc.nextInt(); // Lee el Número de las Columnas
 
-        int [][] matriz = new int [i][j];
+        int [][] matriz = new int [i][j];// Crear la Matriz
 
         sc.close();
 
-        return matriz;
+        return matriz;// Retornar la Matriz
     }
     public void llenarMatriz (int[][] matriz) {
         Random rand = new Random();// Crear objeto Random para Llenar Valores Aleatorios
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                matriz [i][j] = rand.nextInt(101);// Generar Números Aleatorios de 0 a 100
+                matriz [i][j] = rand.nextInt(201) - 100;// Generar Números Aleatorios entre -100 a 100
             }
+        }
+    }
+    public void mostrarMatriz (int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");// Imprimir cada Elemento con Tabulación para alinear
+            }
+            System.out.println();// Nueva línea después de cada fila
         }
     }
 }
